39362696b91b5f53239b20ac63706f9e2c8872d7
danik <stupakdanielo@gmail.com>
2025-12-18 18:22:52 +0200

    Reapply "Redesign ItemSieveForm."
    This reverts commit 0e9f8f9900240822ce3ddfe9d612a3df7f744fc1.

--- RGR1_Project/Models/ItemSieveDTO.cs
+++ RGR1_Project/Models/ItemSieveDTO.cs
@@ -5,13 +5,9 @@
         public ItemPropertyDTO FilterByType { get; }
         public ItemPropertyDTO FilterByText { get; }
         public ItemPropertyDTO FilterByPriority { get; }
-        public ItemPropertyDTO FilterByStatus { get; }
         public ItemPropertyDTO FilterByTime { get; }
+        public ItemPropertyDTO FilterByStatus { get; }
         public ItemPropertyDTO FilterByEndTime { get; }
-
-        public ItemPropertyDTO SortByText { get; }
-        public ItemPropertyDTO SortByPriority { get; }
-        public ItemPropertyDTO SortByStatus { get; }
         public ItemPropertyDTO SortByTime { get; }
         public ItemPropertyDTO SortByEndTime { get; }
 
@@ -20,13 +16,11 @@
             FilterByType = new ItemPropertyDTO(PropertyControlType.ComboBox);
             FilterByText = new ItemPropertyDTO(PropertyControlType.TextBox);
             FilterByPriority = new ItemPropertyDTO(PropertyControlType.ComboBox);
-            FilterByStatus = new ItemPropertyDTO(PropertyControlType.ComboBox);
             FilterByTime = new ItemPropertyDTO(PropertyControlType.DateTimePicker);
-            FilterByEndTime = new ItemPropertyDTO(PropertyControlType.DateTimePicker);
 
-            SortByText = new ItemPropertyDTO(PropertyControlType.ComboBox);
-            SortByPriority = new ItemPropertyDTO(PropertyControlType.ComboBox);
-            SortByStatus = new ItemPropertyDTO(PropertyControlType.ComboBox);
+            FilterByStatus = new ItemPropertyDTO(PropertyControlType.ComboBox);
+            FilterByEndTime = new ItemPropertyDTO(PropertyControlType.DateTimePicker);
+            
             SortByTime = new ItemPropertyDTO(PropertyControlType.ComboBox);
             SortByEndTime = new ItemPropertyDTO(PropertyControlType.ComboBox);
         }
