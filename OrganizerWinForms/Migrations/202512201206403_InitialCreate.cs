4961a7a33bb4c562904fc6f9668b395ba2538ee4
danik <stupakdanielo@gmail.com>
2025-12-20 18:02:59 +0200

    Create a migration of OrganizerItem class
    Helps turn C# data objects to SQL database entries.

---
+++ OrganizerWinForms/Migrations/202512201206403_InitialCreate.cs
@@ -0,0 +1,18 @@
+﻿namespace Organizer_Project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialCreate : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.OrganizerItems", "Type", c => c.Int());
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.OrganizerItems", "Type", c => c.Int(nullable: false));
+        }
+    }
+}
---
+++ OrganizerWinForms/Migrations/Configuration.cs
@@ -0,0 +1,29 @@
+﻿namespace Organizer_Project.Migrations
+{
+    using Organizer_Project.Data;
+    using Organizer_Project.Interfaces;
+    using System.Collections.Generic;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Organizer_Project.Data.OrganizerDbContext>
+    {
+        private readonly int SEED_SAMPLE_SIZE = 5;
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Organizer_Project.Data.OrganizerDbContext";
+        }
+
+        protected override void Seed(Organizer_Project.Data.OrganizerDbContext context)
+        {
+            // Only add data if the database is empty
+            if (!context.Items.Any())
+            {
+                IDataGenerator<OrganizerItem> dataGenerator = new OrganizerItemGenerator();
+                List<OrganizerItem> organizerItems = dataGenerator.GenerateDataList(SEED_SAMPLE_SIZE) as List<OrganizerItem>;
+                context.Items.AddRange(organizerItems);
+            }
+        }
+    }
+}
