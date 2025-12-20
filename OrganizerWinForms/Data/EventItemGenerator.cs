14aac30eaae429ea5a80b21d346418bccdf70497
danik <stupakdanielo@gmail.com>
2025-12-20 17:59:47 +0200

    feat(Service): Implement Task and Event items generators.
    Helps create sample databese entries easier.

---
+++ OrganizerWinForms/Data/EventItemGenerator.cs
@@ -0,0 +1,61 @@
+﻿using Organizer_Project.Interfaces;
+using Organizer_Project.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Organizer_Project.Data
+{
+    public class EventItemGenerator : IDataGenerator<EventItem>
+    {
+        // Fields
+        private static readonly string[] EventTopics =
+        {
+            "Team Sync",
+            "Dentist",
+            "University Lecture",
+            "Lunch with Friend",
+            "Project Review"
+        };
+        private static readonly string[] EventNotes =
+        {
+            "Meeting in Discord.",
+            "Meetimg at office.",
+            "Meeting in 404 audience room.",
+            "Meeting in \"Celentano\" pizzeria.",
+            "Meeting in Figma."
+        };
+        private readonly Random RandomSeed = new Random();
+        private readonly DateTime Time = DateTime.Now.Date;
+        private readonly int EnumPriorityCount = Enum.GetNames(typeof(Priority)).Length;
+        // Properties
+        private string RandomTopic => EventTopics[RandomSeed.Next(0, EventTopics.Length)];
+        private string RandomNote => EventNotes[RandomSeed.Next(0, EventNotes.Length)];
+        // Interface member implementations
+        public EventItem GenerateData()
+        {
+            return GenerateData(RandomSeed.Next());
+        }
+        public EventItem GenerateData(int index)
+        {
+            var startTime = Time.AddHours(RandomSeed.Next(1, 12)).AddDays(RandomSeed.Next(-15, 15)).AddMonths(RandomSeed.Next(-6, 6));
+            return new EventItem
+            (
+                title: $"Event {index}: {RandomTopic}",
+                priority: (Priority)RandomSeed.Next(0, EnumPriorityCount),
+                startTime: startTime,
+                endTime: startTime.AddHours(RandomSeed.Next(1, 12)).AddDays(RandomSeed.Next(0, 3)),
+                notes: RandomNote,
+                isAllDay: RandomSeed.Next(0, 10) > 7 // 20% chance for All Day
+            );
+        }
+        public IEnumerable<EventItem> GenerateDataList(int listCount)
+        {
+            List<EventItem> eventItems = new List<EventItem>();
+            for (int i = 0; i < listCount; i++)
+            {
+                eventItems.Add(GenerateData(i));
+            }
+            return eventItems;
+        }
+    }
+}
---
+++ OrganizerWinForms/Data/TaskItemGenerator.cs
@@ -0,0 +1,60 @@
+﻿using Organizer_Project.Interfaces;
+using Organizer_Project.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Organizer_Project.Data
+{
+    public class TaskItemGenerator : IDataGenerator<TaskItem>
+    {
+        // Fields
+        private static readonly string[] TaskTopics =
+        {
+            "Buy groceries",
+            "Fix bug in C++",
+            "Refactor DTOs",
+            "UI/UX Research",
+            "Finish Report"
+        };
+        private static readonly string[] TaskNotes =
+        {
+            "Eggs, Milk, Coffe, Salad, Sausages.",
+            "Think positively.",
+            "Make right decisions.",
+            "Sleep well.",
+            "Don't forget."
+        };
+        private readonly Random RandomSeed = new Random();
+        private readonly DateTime Time = DateTime.Now.Date;
+        private readonly int EnumPriorityCount = Enum.GetNames(typeof(Priority)).Length;
+        private readonly int EnumStatusCount = Enum.GetNames(typeof(TaskStatus)).Length;
+        // Properties
+        private string RandomTopic => TaskTopics[RandomSeed.Next(0, TaskTopics.Length)];
+        private string RandomNotes => TaskNotes[RandomSeed.Next(0, TaskNotes.Length)];
+        // Interface member implementations
+        public TaskItem GenerateData()
+        {
+            return GenerateData(RandomSeed.Next());
+        }
+        public TaskItem GenerateData(int index)
+        {
+            return new TaskItem
+            (
+                title: $"Task {index}: {RandomTopic}",
+                priority: (Priority)RandomSeed.Next(0, EnumPriorityCount),
+                status: (TaskStatus)RandomSeed.Next(0, EnumStatusCount),
+                endTime: Time.AddHours(RandomSeed.Next(1, 12)).AddDays(RandomSeed.Next(-15, 15)).AddMonths(RandomSeed.Next(-6, 6)),
+                notes: RandomNotes
+            );
+        }
+        public IEnumerable<TaskItem> GenerateDataList(int listCount)
+        {
+            List<TaskItem> taskItems = new List<TaskItem>();
+            for (int i = 0; i < listCount; i++)
+            {
+                taskItems.Add(GenerateData(i));
+            }
+            return taskItems;
+        }
+    }
+}
